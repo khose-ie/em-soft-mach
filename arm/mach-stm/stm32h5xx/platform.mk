7107b8c975eeba827e78dc68475cbfe294513f52
Khose-ie <hyklose@outlook.com>
2025-03-05 11:21:53 +0000

    Use the new include format to include sub makefile.

--- arm/mach-stm/stm32h5xx/platform.mk
+++ arm/mach-stm/stm32h5xx/platform.mk
@@ -1,3 +1,1 @@
-plat-$(CONFIG_PLAT_MODEL_NUCLEO_H563ZI) = arch/arm/mach-stm/stm32h5xx/platform.mk.nucleo-h563zi
-
--include $(plat-y)
+-include $(if $(CONFIG_PLAT_MODEL_NUCLEO_H563ZI), arch/arm/mach-stm/stm32h5xx/platform.mk.nucleo-h563zi)
